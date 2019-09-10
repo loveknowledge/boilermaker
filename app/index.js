fce4b74371752c5668b692046fb2f101c16ee7ec
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-10 19:54:31 -0400

    fixed react issue

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './yourStore';
 
 ReactDOM.render(
-  <div>Hello, world!</div>,
-  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+  <Provider store={store}>{/* rest of your app goes here! */}</Provider>,
+  document.getElementById('yourApp')
 );
