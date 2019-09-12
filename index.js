803ba4813d17c0087bb104599f31f50d50bb362f
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-12 12:21:37 -0400

    had a bug in my database file, improper link to my db, fixed that

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const app = require('./server');
+const db = require('./server/db/database');
 
 // Server
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Knock, knock');
-  console.log("Who's there?");
-  console.log(`Your server, listening on port ${port}`);
-});
+
+db.sync() // sync our database
+  .then(function() {
+    app.listen(port); // then start listening with our express server once we have synced
+  });
+
+// app.listen(port, function() {
+//   console.log('Knock, knock');
+//   console.log("Who's there?");
+//   console.log(`Your server, listening on port ${port}`);
+// });
--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432:johnnydb',
+  process.env.DATABASE_URL || 'postgres://localhost:5432/johnnydb',
   {
     logging: false, // unless you like the logs
     // ...and there are many other options you may want to play with
